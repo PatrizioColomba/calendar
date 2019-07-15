2743969870b22048cf76a5085b5e71e2ab191f90
PatrizioC <patrizio.colomba@gmail.com>
2019-07-15 11:44:33 +0200

    Resolve issue #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calendar';
+   date = formatDate(new Date().toDateString(), 'dd MMMM yyyy', 'en');
+   title = 'calendar';
 }
