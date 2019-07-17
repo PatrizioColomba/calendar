f43901c6e24d10a77d7ca24cb2cb2feb6e6b3f35
PatrizioC <patrizio.colomba@gmail.com>
2019-07-17 19:13:38 +0200

    Adds week component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,39 +2,44 @@ import { Component } from '@angular/core';
 import { formatDate } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   date: Date;
-   dateStr: string;
-   title = 'calendar';
+	date: Date;
+	dateStr: string;
+	title = 'calendar';
 
-   constructor() {
+	constructor() {
 		this.date = new Date();
-      this.dateStr = formatDate(
-			this.date.toDateString(), 'dd MMMM yyyy', 'en');
-   }
+		this.dateStr = formatDate(
+		this.date.toDateString(), 'dd MMMM yyyy', 'en');
+	}
 
-   onClick(cmd: string) {
+	onClick(cmd: string) {
 		let dayNumber: number = this.date.getDate();
-		switch(cmd) 
-		{
-			case "prev":
-				dayNumber--;
-			break;
+		switch (cmd) {
 
-			case "next":
+			case 'prev':
+				dayNumber--;
+				break;
+
+			case 'next':
 				dayNumber++;
-			break;
+				break;
 
 			default:
-			break;
+				break;
 		}
-	   this.date = new Date(
-		   this.date.getFullYear(),
-		   this.date.getMonth(), dayNumber);
-	   this.dateStr = formatDate(this.date, 'dd MMMM yyyy', 'en');
-   }
+		this.date = new Date(
+			this.date.getFullYear(),
+			this.date.getMonth(), dayNumber);
+		this.dateStr = formatDate(this.date, 'dd MMMM yyyy', 'en');
+	}
+
+	changeView() {
+		let i = 0;
+		i++;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeekComponent } from './week/week.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeekComponent
   ],
   imports: [
     BrowserModule
