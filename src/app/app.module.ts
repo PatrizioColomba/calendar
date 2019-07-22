ebc3aa6b122cc86f0bb69a1b6c2462f013511292
PatrizioC <patrizio.colomba@gmail.com>
2019-07-22 11:53:11 +0200

    Adds app routing and month component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WeekComponent } from './week/week.component';
 import { ControlsComponent } from './controls/controls.component';
+import { CalendarService } from './calendar.service';
+import { MonthComponent } from './month/month.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		WeekComponent,
-		ControlsComponent
+		ControlsComponent,
+		MonthComponent
 	],
 	imports: [
-		BrowserModule
+		RouterModule.forRoot([
+			{
+				path: '',
+				redirectTo: '/week',
+				pathMatch: 'full'
+			},
+			{
+				path: 'week',
+				component: WeekComponent,
+			},
+			{
+				path: 'month',
+				component: MonthComponent,
+			}
+		]),
+		BrowserModule,
 	],
-	providers: [],
+	providers: [
+		CalendarService
+	],
 	bootstrap: [AppComponent]
 })
+
 export class AppModule { }
