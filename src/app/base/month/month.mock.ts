c25da0c358ab4a249f3fe77e6efd8819b9c65635
PatrizioC <patrizio.colomba@gmail.com>
2019-11-17 13:44:13 +0100

    Fixes compatibility issues.

---
+++ src/app/base/month/month.mock.ts
@@ -0,0 +1,5 @@
+export class MonthMock {
+  static MONTH_NUMBER: number = 5; //May
+  static YEAR: number = 2020;
+  static FIRST_WEEK_MONDAY_DAY: number = 27;
+}
--- src/app/base/month/month.spec.ts
+++ src/app/base/month/month.spec.ts
@@ -1,38 +1,36 @@
 import { Month } from './month';
 import { Week } from './../week/week';
-
-const MONTH: number = 1;
-const YEAR: number = 2022;
+import { MonthMock } from './month.mock';
 
 describe('Month', () => {
   it('should create an instance', () => {
-    let month = new Month(MONTH, YEAR);
+    let month = new Month(MonthMock.MONTH_NUMBER, MonthMock.YEAR);
     expect(month).toBeTruthy();
   });
 
   it('should set a mew value for month and create an instance', () => {
-    let month = new Month(MONTH, YEAR);
-    month = month.setMonth(MONTH+1);
+    let month = new Month(MonthMock.MONTH_NUMBER, MonthMock.YEAR);
+    month = month.setMonth(MonthMock.MONTH_NUMBER+1);
     expect(month).toBeTruthy();
   });
 
   it('should set a new value for year and create an instance', () => {
-    let month = new Month(MONTH, YEAR);
-    month = month.setYear(YEAR+1);
+    let month = new Month(MonthMock.MONTH_NUMBER, MonthMock.YEAR);
+    month = month.setYear(MonthMock.YEAR+1);
     expect(month).toBeTruthy();
   });
 
   it('should return a Monday', () => {
-    let month = new Month(MONTH, YEAR);
-    month = month.setYear(YEAR+1);
+    let month = new Month(MonthMock.MONTH_NUMBER, MonthMock.YEAR);
+    month = month.setYear(MonthMock.YEAR+1);
     let week: Week = month.weeks()[0];
     let day: Date = week.days()[0];
     expect(day.getDay()).toEqual(1);
   });
 
   it('should return a Sunday', () => {
-    let month = new Month(MONTH, YEAR);
-    month = month.setYear(YEAR+1);
+    let month = new Month(MonthMock.MONTH_NUMBER, MonthMock.YEAR);
+    month = month.setYear(MonthMock.YEAR+1);
     let weeksLen = month.weeks().length;
     let week: Week = month.weeks()[weeksLen-1];
     let day: Date = week.days()[6];
